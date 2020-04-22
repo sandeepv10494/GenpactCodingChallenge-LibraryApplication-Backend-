72364d8c54fc3a6f2e8abc7d70021505c82cd78c
Sandeep V <45992565+sandeepv10494@users.noreply.github.com>
2020-04-22 11:54:42 +0530

    Added sql file to insert data to H2 DB

---
+++ Library/src/main/resources/import.sql
@@ -0,0 +1,26 @@
+#Inserting Library Data
+insert into libray values (1, 'Public Library);
+insert into libray values (2, 'City Central Library);
+insert into libray values (3, 'Nool Library);
+insert into libray values (4, 'British Council Library);
+
+#Inserting Book Data
+insert into book values (1, 'Glimpses of World History', 'Jawaharlal Nehru', 1);
+insert into book values (2, 'Prison Diary', 'Jay Prakash Narayan', 1);
+insert into book values (3, 'Geet Govind', 'Jaya Dev', 1);
+insert into book values (4, 'Why Socialism', 'Jayaprakash Narayan', 1);
+
+insert into book values (5, 'Glimpses of World History', 'Jawaharlal Nehru', 2);
+insert into book values (6, 'Prison Diary', 'Jay Prakash Narayan', 2);
+insert into book values (7, 'Geet Govind', 'Jaya Dev', 2);
+insert into book values (8, 'Why Socialism', 'Jayaprakash Narayan', 2);
+
+insert into book values (9, 'Glimpses of World History', 'Jawaharlal Nehru', 3);
+insert into book values (10, 'Prison Diary', 'Jay Prakash Narayan', 3);
+insert into book values (11, 'Geet Govind', 'Jaya Dev', 3);
+insert into book values (12, 'Why Socialism', 'Jayaprakash Narayan', 3);
+
+insert into book values (13, 'Glimpses of World History', 'Jawaharlal Nehru', 4);
+insert into book values (14, 'Prison Diary', 'Jay Prakash Narayan', 4);
+insert into book values (15, 'Geet Govind', 'Jaya Dev', 4);
+insert into book values (16, 'Why Socialism', 'Jayaprakash Narayan', 4);
